2afc3b5d2829c289bcd9e6053fbe0546ed15efe2
Anujit-Datta <anujitbestbd@gmail.com>
2024-09-26 04:02:27 +0600

    messaging screen ui refactored

--- lib/presentation/screens/messaging_screen.dart
+++ lib/presentation/screens/messaging_screen.dart
@@ -1,5 +1,4 @@
 import 'package:chatsapp/presentation/utils/app_colors.dart';
-import 'package:chatsapp/presentation/widgets/camera_button.dart';
 import 'package:chatsapp/presentation/widgets/messaging/appbar.dart';
 import 'package:chatsapp/presentation/widgets/messaging/background.dart';
 import 'package:flutter/material.dart';
@@ -21,69 +20,115 @@ class MessagingScreen extends StatelessWidget {
               children: [
                 SizedBox(
                   width: MediaQuery.of(context).size.width - 50,
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 0),
-                      decoration: BoxDecoration(
-                        color: AppColors.messageBG,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.emoji_emotions_outlined,
-                              color: Colors.white24,
-                            ),
-                          ),
-                          const Expanded(
-                            child: TextField(
-                              decoration: InputDecoration(
-                                hintText: 'Type a message',
-                                hintStyle: TextStyle(
-                                  color: Colors.white24,
-                                ),
-                                border: InputBorder.none,
-                              ),
-                              maxLines: 10,
-                              minLines: 1,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.attach_file,
-                              color: Colors.white24,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.camera_alt_outlined,
-                              color: Colors.white24,
-                            ),
-                          ),
-                        ],
-                      ),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 0),
+                    decoration: BoxDecoration(
+                      color: AppColors.messageBG,
+                      borderRadius: BorderRadius.circular(30),
                     ),
-                ),
-                CircleAvatar(
-                  backgroundColor: AppColors.primary,
-                  radius: 23,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.send,
-                      color: Colors.black,
-                      size: 22,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        const ImojiButton(),
+                        messageInputField(),
+                        const AttachmentButton(),
+                        const CameraButton(),
+                      ],
                     ),
                   ),
                 ),
+                sendButton(),
               ],
             )
           ],
         ),
+      ),
+    );
+  }
+
+  Expanded messageInputField() {
+    return const Expanded(
+      child: TextField(
+        decoration: InputDecoration(
+          focusColor: Colors.white,
+          hintText: 'Message',
+          hintStyle: TextStyle(
+            color: Colors.white24,
+            fontSize: 16
+          ),
+          border: InputBorder.none,
+        ),
+        maxLines: 10,
+        minLines: 1,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  CircleAvatar sendButton() {
+    return CircleAvatar(
+      backgroundColor: AppColors.primary,
+      radius: 23,
+      child: IconButton(
+        onPressed: () {},
+        icon: const Icon(
+          Icons.send,
+          color: Colors.black,
+          size: 22,
+        ),
+      ),
+    );
+  }
+}
+
+class CameraButton extends StatelessWidget {
+  const CameraButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {},
+      icon: const Icon(
+        Icons.camera_alt_outlined,
+        color: Colors.white24,
+      ),
+    );
+  }
+}
+
+class AttachmentButton extends StatelessWidget {
+  const AttachmentButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {},
+      icon: const Icon(
+        Icons.attach_file,
+        color: Colors.white24,
+      ),
+    );
+  }
+}
+
+class ImojiButton extends StatelessWidget {
+  const ImojiButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {},
+      icon: const Icon(
+        Icons.emoji_emotions_outlined,
+        color: Colors.white24,
       ),
     );
   }
